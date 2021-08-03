58de7f245c98639c70280fc36d36d12fbb8f844d
seohyunsim <ssh4563@naver.com>
2021-08-03 14:39:34 +0900

    #1 PURE REDUX: COUNTER

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+number.innerText = 0;
 
-number.innerHTML = count;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
 
-const updateCount = () => {
-  number.innerHTML = count;
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 }
 
-const handleAdd = () => {
-  count++;
-  updateCount();
-};
+countStore.subscribe(onChange);
 
-const handleMinus = () => {
-  count--;
-  updateCount();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener('click', () => countStore.dispatch({ type: ADD }));
+minus.addEventListener('click', () => countStore.dispatch({ type: MINUS }));
