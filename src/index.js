ce57f4d9632dcddf44dbf7720f04f2ee2b01b06a
seohyunsim <ssh4563@naver.com>
2021-08-09 17:13:38 +0900

    #4.1 createAction (06:16)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import App from './Components/App';
-// import store from './store';
-import configureStore from './store';
+import store from './store';
+import { Provider } from 'react-redux';
 
-const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate persistor={persistor}>
       <App />
-    </PersistGate>
   </Provider>
   , document.getElementById('root'));
--- src/store.js
+++ src/store.js
@@ -1,50 +1,30 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; 
+import { createAction } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
+const addToDo = createAction('ADD');
+const deleteToDo = createAction('DELETE');
 
-const addToDo = text => {
-    return {
-        type: ADD,
-        text
-    }
-};
-
-const deleteToDo = id => {
-    return {
-        type: DELETE,
-        id: parseInt(id)
-    }
-};
+// localStorage에 저장하기
+// const defaultState = {
+//     items: JSON.parse(localStorage.getItem('items')) || []
+// }; 
 
 const reducer = (state = [], action) => {
     switch (action.type) {
-        case ADD:
-            return [...state, { text: action.text, id: Date.now() }];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
+        case addToDo.type:
+            return [...state, { text: action.payload, id: Date.now() }];
+        case deleteToDo.type:
+            return state.filter(toDo => toDo.id !== action.payload);
         default:
             return state;
     }
 };
 
-const persistConfig = {
-    key: 'root',
-    storage
-};
-
-const persistedState = persistReducer(persistConfig, reducer);
-
+const store = createStore(reducer);
 
 export const actionCreators = {
     addToDo,
     deleteToDo
 };
 
-export default function configureStore() {
-    const store = createStore(persistedState);
-    const persistor = persistStore(store);
-    return { store, persistor };
-}
+export default store;
